ba0e47eed17a125a6d9fc11841937d6fb6171fe8
HarperReinhardt <harperreinhardt@gmail.com>
2026-05-13 20:29:24 -0500

    added airsim camera stream

---
+++ hawkspot/streams/ogAirsim_camera.py
@@ -0,0 +1,32 @@
+try:
+    import airsim
+except ImportError as exc:
+    raise ImportError(
+        "Airsim library is not installed. Please install it to use the ogAirsim_camera stream."
+    ) from exc
+
+from hawkspot import BaseStream
+
+
+from hawkspot import BaseStream
+
+
+class AirsimCamera(BaseStream):
+    """
+    Sets up a connected Airsim camera to be used as a stream.
+    """
+
+    def __init__(self, showStream: bool = False, airsim_ip: str = "127.0.0.1", camera_name: str = "bottom_center"):
+
+        super().__init__(showStream)
+        self._client = airsim.VehicleClient()
+        self._client.confirmConnection()
+
+    def get_frame(self):
+        self.frame = self._client.simGetImage("bottom_center", airsim.ImageType.Scene)
+        #self.frame = self._camera.capture_array()
+        super().show_frame()
+        return self.frame
+
+    def end(self):
+        self._camera.stop_recording()
