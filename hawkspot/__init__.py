8c420c5d46c48c9bd66de99410208eaa462aaca3
HarperReinhardt <harperreinhardt@gmail.com>
2026-05-14 01:33:54 -0500

    Remove detector and stream imports because they were giving me troubleee

--- hawkspot/__init__.py
+++ hawkspot/__init__.py
@@ -8,15 +8,7 @@ from hawkspot.base import (
     DetectionOffset,
     LocalOffset,
 )
-from hawkspot.detectors.april_tag import AprilTagDetector
-from hawkspot.detectors.rt_detr import RTDETRV2_Detector 
-from hawkspot.streams.ogAirsim_camera import AirsimCamera
-current_os = platform.system()
-if current_os == "Linux":
-    from hawkspot.streams.rpi_camera import RPICamera
 
-from hawkspot.streams.siyi_camera import SiyiCamera
-from hawkspot.detectors.april_tag import AprilTagDetector
 from hawkspot.drone import Drone
 from hawkspot.hawkspot import Hawkspot
 
