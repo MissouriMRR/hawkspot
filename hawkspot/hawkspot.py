d985e69144f0d5255a120ed223e28c19ba6f9784
HarperReinhardt <harperreinhardt@gmail.com>
2026-05-14 07:58:27 -0500

    I may not have a brain gentlemen, but I have an idea

--- hawkspot/hawkspot.py
+++ hawkspot/hawkspot.py
@@ -168,8 +168,8 @@ class Hawkspot:
                 # Start moving to point
                 if not landing:
                     point = dronekit.LocationLocal(offset.forward, offset.right, 0)
-                    for i in range(10):
-                        self.drone.goto_local(point)
+                    
+                    self.drone.goto_local(point)
                 
 
                 # Send LANDING_TARGET message
